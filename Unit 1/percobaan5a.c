39daf9ff665ac34da1b288e0308e2f815d5ce4ad
FawwazFatchurr <161607759+FawwazFatchurr@users.noreply.github.com>
2024-03-02 21:27:45 +0700

    Create percobaan5a.c

---
+++ Unit 1/percobaan5a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char fullname[30];
+
+int main(){
+    printf("Masukan nama lengkap kamu: \n");
+    scanf("%s", fullname);
+    printf("Halo %s", fullname);
+    return 0;
+}
+
+
+//untuk program a hanya mengambil nama depan dari inputan
