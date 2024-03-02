7c7c812135fee347b69b2eb29eeb76ecb560559d
FawwazFatchurr <161607759+FawwazFatchurr@users.noreply.github.com>
2024-03-02 20:17:51 +0700

    Create percobaan3a.c

---
+++ Unit 1/percobaan3a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  printf("Baris pertama");
+  printf("Baris kedua \n");
+  printf("Baris ketiga");
+  return 0;
+}
+
+//outputnya akan menjadi 
+
+//Baris pertama Baris kedua
+//Baris ketiga
