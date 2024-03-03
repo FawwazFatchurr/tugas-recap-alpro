8358538673e8773a15acf4d851faff0d3cd88655
FawwazFatchurr <161607759+FawwazFatchurr@users.noreply.github.com>
2024-03-03 12:46:37 +0700

    Create percobaan4b.c

---
+++ Unit 2/percobaan4b.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int i, j;
+
+int main(){
+    
+    for (i = 2; i <= 3; ++i) {
+        printf("Bilangan utama: %d\n", i);
+        
+        for (j = 1; j <= 2; ++j) {
+            printf(" Bilangan pengali: %d\n", j);
+            printf("  Hasil perkalian: %d\n\n", j * i);
+            }
+        
+    }
+
+    return 0;
+}
+
+// program tersebut mengerjakan perintah for yang di dalam for terlebih dahulu kemudian apabila sudah mentok barulah program for yang sebagai bilangan utama di tambahkan 1
