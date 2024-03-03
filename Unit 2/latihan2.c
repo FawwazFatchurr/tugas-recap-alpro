f559a87d69cb3272158cc051f4fb97f627c3f800
FawwazFatchurr <161607759+FawwazFatchurr@users.noreply.github.com>
2024-03-03 20:33:55 +0700

    Update latihan2.c

--- Unit 2/latihan2.c
+++ Unit 2/latihan2.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int jumlah;
+    int nominal;
+    int total = 0;
+    
+    // jumlah transaksi
+    printf("Masukkan jumlah transaksi: ");
+    scanf("%d", &jumlah);
+    
+    
+    if (jumlah <= 0) {
+        printf("Tidak ada transaksi hari ini\n");
+        return 0;
+    }
+    
+    // menghitung total pengeluaran
+    for (int i = 0; i < jumlah; i++) {
+        printf("Nominal transaksi ke-%d: ", i + 1);
+        scanf("%d", &nominal);
+        total += nominal;
+    }
+    
+    // total pengeluaran
+    printf("Total hari ini: %d\n", total);
+    
+    return 0;
+}
 
